881e02255a2835beaa1ee56f761d24d951da5dbc
marcos-inja <marcosvaas@gmail.com>
2022-02-14 19:20:40 -0300

    feat: getting all the jokes on the site using only the main url

--- scraping.py
+++ scraping.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import concurrent.futures
+from threading import Thread
 import json
 
 from bs4 import BeautifulSoup
@@ -34,26 +35,54 @@ def get_joke(url: str) -> str:
     joke = joke.replace("\r\n", " ").replace("\n", " ").replace("\r", " ")
     return joke
 
-with open("jokes.json", "r") as fp:
-    jokes = json.load(fp)
-    
 
-def get_page_joke(init: str) -> None:
+    
+def get_page_joke(init: str) -> list:
     joke_list = get_links(URL_BASE + init)
     with concurrent.futures.ThreadPoolExecutor() as executor:
-            file = []
-            for joke_link in joke_list:
-                file.append(
-                    executor.submit(
-                        get_joke, URL_BASE + joke_link
-                    )
+        file = []
+        jokes = []
+        for joke_link in joke_list:
+            file.append(
+                executor.submit(
+                    get_joke, URL_BASE + joke_link
                 )
-            for future in concurrent.futures.as_completed(file):
-                jokes.append(future.result())
+            )
+        for future in concurrent.futures.as_completed(file):
+            jokes.append(future.result())
+
+    return jokes
 
 
-PAGE_INIT = "piadas-de-casais.htm"
-get_page_joke(PAGE_INIT)
-    
-with open(f"jokes.json", "w") as fp:
-    json.dump(jokes, fp, indent=4, ensure_ascii=False)
+def get_links_all_categories() -> list:
+    page = get_url_soup(URL_BASE)
+
+    links = page.find(class_ = "menuEsq")
+    tag_a = links.find_all("a")
+    link_categories = []
+    for link in tag_a:
+        link_categories.append(link.attrs["href"])
+    return link_categories
+
+
+links_categories = get_links_all_categories()
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    file = []
+    for link in links_categories:
+        file.append(
+            executor.submit(
+                get_page_joke, link
+            )
+        )
+
+    for future in concurrent.futures.as_completed(file):
+        with open(f"jokes.json", "r") as fp:
+            jokes_file = json.load(fp)
+
+        for joke in future.result():
+            jokes_file.append(joke)
+
+        with open(f"jokes.json", "w") as fp:
+            json.dump(jokes_file, fp, indent=4, ensure_ascii=False)
